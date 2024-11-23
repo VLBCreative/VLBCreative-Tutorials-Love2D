89d0975982b40ba157d8c956f61e7ff41309bee6
vbushong16 <bushong.victor@gmail.com>
2024-11-23 11:38:19 -0800

    Adding game animation

--- basic/main.lua
+++ basic/main.lua
@@ -1,4 +1,6 @@
 
+Class = require 'class'
+
 WINDOW_HEIGHT = 800
 WINDOW_WIDTH = 800
 
@@ -6,7 +8,8 @@ BOX_SIZE = 100
 x = WINDOW_WIDTH-BOX_SIZE
 y = 0
 dy = 0
-GRAVITY = 10
+GRAVITY = 15
+PLAYER_MOV = 5
 
 py = 50
 px = 50
@@ -19,6 +22,27 @@ function love.load()
         fullscreen = false,
         resizable = false
     })
+
+    spritesheet = love.graphics.newImage('SkiFree_-_WIN3_-_Sprite_Sheet.png')
+    gFrames = {
+        ['yeti'] = {
+            love.graphics.newQuad(10,52,33,41,spritesheet:getDimensions()),
+        love.graphics.newQuad(44,50,29,43,spritesheet:getDimensions()),
+        love.graphics.newQuad(74,49,26,44,spritesheet:getDimensions()),
+        love.graphics.newQuad(101,54,31,39,spritesheet:getDimensions()),
+        }
+    }
+
+    idleYeti = Animation{
+        frames = {1,2},
+        interval = 0.2
+    }
+    runningYeti = Animation{
+        frames = {3,4},
+        interval = 0.2
+    }
+
+    yetiAnim = idleYeti
 
 end
 
@@ -37,7 +61,9 @@ function love.keypressed(key)
 
 end
 
-function love.update()
+function love.update(dt)
+
+    yetiAnim:update(dt)
 
     if y<= 0 then
         dy = GRAVITY
@@ -48,16 +74,19 @@ function love.update()
     y = y+dy
 
     if love.keyboard.isDown('down') then
-        py = py + GRAVITY
-    end
-    if love.keyboard.isDown('up') then
-        py = py - GRAVITY
-    end
-    if love.keyboard.isDown('left') then
-        px = px - GRAVITY
-    end
-    if love.keyboard.isDown('right') then
-        px = px + GRAVITY
+        py = py + PLAYER_MOV
+        yetiAnim = runningYeti
+    elseif love.keyboard.isDown('up') then
+        py = py - PLAYER_MOV
+        yetiAnim = runningYeti
+    elseif love.keyboard.isDown('left') then
+        px = px - PLAYER_MOV
+        yetiAnim = runningYeti
+    elseif love.keyboard.isDown('right') then
+        px = px + PLAYER_MOV
+        yetiAnim = runningYeti
+    else
+        yetiAnim = idleYeti
     end
 
     if py >= y and py <= y+BOX_SIZE then
@@ -74,7 +103,7 @@ function love.update()
         projy = py
     end
 
-    if projx == WINDOW_WIDTH then
+    if projx >= WINDOW_WIDTH then
         if px > WINDOW_WIDTH/2 then
             pscore = pscore + 1
         else
@@ -101,16 +130,45 @@ end
 
 function love.draw()
 
+    love.graphics.setColor(255,255,255)
+    love.graphics.rectangle('fill',0,0,WINDOW_WIDTH,WINDOW_HEIGHT)
+    
+    love.graphics.setColor(0,0,0)
     love.graphics.rectangle('fill',x,y,BOX_SIZE,BOX_SIZE)
-    love.graphics.circle('fill',px,py,10)
     love.graphics.rectangle('fill',projx,projy,5,10)
-
     love.graphics.line(WINDOW_WIDTH/2,0,WINDOW_WIDTH/2,WINDOW_HEIGHT)
-
     love.graphics.printf('Score: ' ..tostring(pscore),0,0,WINDOW_WIDTH)
     love.graphics.printf('2pt Area',WINDOW_WIDTH/4 -10,WINDOW_HEIGHT/2,WINDOW_WIDTH)
     love.graphics.printf('1pt Area',WINDOW_WIDTH*3/4 -10,WINDOW_HEIGHT/2,WINDOW_WIDTH)
-    
+    love.graphics.reset()
+
+    love.graphics.draw(spritesheet,gFrames['yeti'][yetiAnim:getFrame()],px,py,0,2,2)
+
+end
 
 
+Animation = Class{}
+
+function Animation:init(def)
+
+    self.frames = def.frames
+    self.interval = def.interval
+    self.timer = 0
+    self.currentFrame = 1
+
+end
+
+function Animation:update(dt)
+
+    if #self.frames > 1 then
+        self.timer = self.timer +dt
+        if self.timer > self.interval then
+            self.timer = self.timer % self.interval
+            self.currentFrame = math.max(1, (self.currentFrame+1) % (#self.frames+1))
+        end
+    end
+end
+
+function Animation:getFrame()
+    return self.frames[self.currentFrame]
 end
