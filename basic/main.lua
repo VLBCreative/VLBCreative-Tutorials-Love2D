f522f0649ab726a7f3a67e83b06a5d69fea274b3
vbushong16 <bushong.victor@gmail.com>
2024-12-05 16:10:28 -0800

    Basic Game update - Skier and Physics

--- basic/main.lua
+++ basic/main.lua
@@ -9,12 +9,19 @@ x = WINDOW_WIDTH-BOX_SIZE
 y = 0
 dy = 0
 GRAVITY = 15
-PLAYER_MOV = 5
+PLAYER_MOV = 200
+SKIER_MOV = 100
 
-py = 50
-px = 50
+py = WINDOW_HEIGHT/2
+px = WINDOW_WIDTH/2
 
 pscore = 0
+
+skierx = 0
+skiery = 200
+
+skier_table = {}
+skier_fixture = {}
 
 function love.load()
 
@@ -30,7 +37,22 @@ function love.load()
         love.graphics.newQuad(44,50,29,43,spritesheet:getDimensions()),
         love.graphics.newQuad(74,49,26,44,spritesheet:getDimensions()),
         love.graphics.newQuad(101,54,31,39,spritesheet:getDimensions()),
-        }
+        love.graphics.newQuad(133,51,33,42,spritesheet:getDimensions()),
+        love.graphics.newQuad(167,51,30,42,spritesheet:getDimensions()),
+        love.graphics.newQuad(198,51,30,42,spritesheet:getDimensions()),
+        love.graphics.newQuad(229,51,24,42,spritesheet:getDimensions()),
+        love.graphics.newQuad(254,55,27,38,spritesheet:getDimensions()),
+        love.graphics.newQuad(282,55,27,38,spritesheet:getDimensions())},
+
+        ['skier']={love.graphics.newQuad(10,12,23,27,spritesheet:getDimensions()),
+        love.graphics.newQuad(34,10,23,29,spritesheet:getDimensions()),
+        love.graphics.newQuad(58,7,17,32,spritesheet:getDimensions())}
+        
+    }
+
+    eatingYeti = Animation{
+        frames = {5,6,7,8,9,10},
+        interval =0.2
     }
 
     idleYeti = Animation{
@@ -44,6 +66,12 @@ function love.load()
 
     yetiAnim = idleYeti
 
+    world = love.physics.newWorld(0,0)
+
+    yetiBody = love.physics.newBody(world,px,py,'dynamic')
+    yetiShape = love.physics.newRectangleShape(50,50)
+    yetiFixture = love.physics.newFixture(yetiBody,yetiShape)
+
 end
 
 function love.keypressed(key)
@@ -53,9 +81,28 @@ function love.keypressed(key)
     end
 
     if key == 'space' then
-        proj = true
-        projx = px
-        projy = py
+        -- proj = true
+        -- projx = px
+        -- projy = py
+
+        table.insert(skier_table,{
+            ['skierBody'] = love.physics.newBody(world,0,math.random(50, WINDOW_HEIGHT - 50),'dynamic'),
+            ['skierShape'] = love.physics.newRectangleShape(25,25),
+            ['isMoving'] = false
+        })
+
+        for i in pairs(skier_table) do
+            table.insert(skier_fixture,{
+                ['skierFixture'] = love.physics.newFixture(skier_table[i]['skierBody'],skier_table[i]['skierShape'])
+            })
+            if skier_table[i]['isMoving'] == false then
+                skier_table[i]['skierBody']:setLinearVelocity(SKIER_MOV,0)
+                skier_table[i]['isMoving'] = true
+            end
+        end
+
+
+
     end
     
 
@@ -64,6 +111,11 @@ end
 function love.update(dt)
 
     yetiAnim:update(dt)
+    world:update(dt)
+
+    py = yetiBody:getY()
+    px = yetiBody:getX()
+
 
     if y<= 0 then
         dy = GRAVITY
@@ -74,18 +126,28 @@ function love.update(dt)
     y = y+dy
 
     if love.keyboard.isDown('down') then
-        py = py + PLAYER_MOV
+        yetiBody:setLinearVelocity(0,PLAYER_MOV)
+        -- py = py + PLAYER_MOV
         yetiAnim = runningYeti
     elseif love.keyboard.isDown('up') then
-        py = py - PLAYER_MOV
+        yetiBody:setLinearVelocity(0,-PLAYER_MOV)
+        -- py = py - PLAYER_MOV
         yetiAnim = runningYeti
     elseif love.keyboard.isDown('left') then
-        px = px - PLAYER_MOV
+        yetiBody:setLinearVelocity(-PLAYER_MOV,0)
+
+        -- px = px - PLAYER_MOV
         yetiAnim = runningYeti
+        left_direction  = true
     elseif love.keyboard.isDown('right') then
-        px = px + PLAYER_MOV
+        yetiBody:setLinearVelocity(PLAYER_MOV,0)
+
+        -- px = px + PLAYER_MOV
         yetiAnim = runningYeti
+        left_direction = false
     else
+        yetiBody:setLinearVelocity(0,0)
+    
         yetiAnim = idleYeti
     end
 
@@ -125,6 +187,14 @@ function love.update(dt)
         end
     end
 
+    if skiery+50 >= py and skiery <= py+50 then
+        if skierx+50 >= px and skierx <= px+50 then
+            skierx = skierx
+            yetiAnim = eatingYeti
+        end
+    else
+        skierx = skierx + PLAYER_MOV
+    end
 
 end
 
@@ -140,9 +210,41 @@ function love.draw()
     love.graphics.printf('Score: ' ..tostring(pscore),0,0,WINDOW_WIDTH)
     love.graphics.printf('2pt Area',WINDOW_WIDTH/4 -10,WINDOW_HEIGHT/2,WINDOW_WIDTH)
     love.graphics.printf('1pt Area',WINDOW_WIDTH*3/4 -10,WINDOW_HEIGHT/2,WINDOW_WIDTH)
+
+    -- love.graphics.rectangle('line',px,py,50,50)
+    -- love.graphics.rectangle('line',skierx,skiery,50,50)
     love.graphics.reset()
 
-    love.graphics.draw(spritesheet,gFrames['yeti'][yetiAnim:getFrame()],px,py,0,2,2)
+    offsetx_yeti = select(3,gFrames['yeti'][1]:getViewport())/2
+    offsety_yeti = select(4,gFrames['yeti'][1]:getViewport())/2
+
+    -- love.graphics.draw(spritesheet,gFrames['skier'][1],skierx,skiery,0,2,2)
+
+
+    for i in pairs(skier_table) do
+        love.graphics.draw(spritesheet,gFrames['skier'][1],skier_table[i]['skierBody']:getX(),skier_table[i]['skierBody']:getY(),0,2,2)
+    end
+
+    love.graphics.draw(spritesheet,gFrames['yeti'][yetiAnim:getFrame()],yetiBody:getX(),yetiBody:getY(),0,left_direction == true and -2 or 2,2,offsetx_yeti,offsety_yeti)
+    -- love.graphics.draw()
+
+
+    -- love.graphics.setColor(0,0,0)
+    -- for _, body in pairs(world:getBodies()) do
+    --     for _, fixture in pairs(body:getFixtures()) do
+    --         local shape = fixture:getShape()
+    
+    --         if shape:typeOf("CircleShape") then
+    --             local cx, cy = body:getWorldPoints(shape:getPoint())
+    --             love.graphics.circle("line", cx, cy, shape:getRadius())
+    --         elseif shape:typeOf("PolygonShape") then
+    --             love.graphics.polygon("line", body:getWorldPoints(shape:getPoints()))
+    --         else
+    --             love.graphics.line(body:getWorldPoints(shape:getPoints()))
+    --         end
+    --     end
+    -- end
+    -- love.graphics.reset( )
 
 end
 
